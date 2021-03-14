9caf7fe2b9b3feefc10b50817d71f58603813d89
pig25 <relife25jw@gmail.com>
2021-03-14 17:56:05 +0800

    加入專案檔案。

---
+++ Project_Report_View/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Project_Report_View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tslb_Font = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tslB_Refresh = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 400);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslb_Font,
+            this.toolStripSeparator1,
+            this.tslB_Refresh});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 50);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tslb_Font
+            // 
+            this.tslb_Font.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tslb_Font.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tslb_Font.Image = ((System.Drawing.Image)(resources.GetObject("tslb_Font.Image")));
+            this.tslb_Font.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tslb_Font.Name = "tslb_Font";
+            this.tslb_Font.Size = new System.Drawing.Size(159, 47);
+            this.tslb_Font.Text = "設定字型";
+            this.tslb_Font.Click += new System.EventHandler(this.tslb_Font_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 50);
+            // 
+            // tslB_Refresh
+            // 
+            this.tslB_Refresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tslB_Refresh.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tslB_Refresh.Image = ((System.Drawing.Image)(resources.GetObject("tslB_Refresh.Image")));
+            this.tslB_Refresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tslB_Refresh.Name = "tslB_Refresh";
+            this.tslB_Refresh.Size = new System.Drawing.Size(227, 47);
+            this.tslB_Refresh.Text = "更新報表資料";
+            this.tslB_Refresh.Click += new System.EventHandler(this.tslB_Refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tslb_Font;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tslB_Refresh;
+    }
+}
+
---
+++ Project_Report_View/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project_Report_View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Font = Properties.Settings.Default.Font;
+            dataGridView1.RowHeadersDefaultCellStyle.BackColor = this.BackColor;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = this.BackColor;
+            dataGridView1.DefaultCellStyle.BackColor = this.BackColor;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            
+            dataGridView1.BackgroundColor= this.BackColor;
+            SetFont();
+
+            tslB_Refresh_Click(null,null);
+        }
+        private void SetFont()
+        {  
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = this.Font;
+            dataGridView1.DefaultCellStyle.Font = this.Font;
+            dataGridView1.AutoResizeRows();
+            dataGridView1.AutoResizeColumns();
+        }
+        private void tslb_Font_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.Font = this.Font;
+            if(fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.Font = fontDialog.Font;
+                Properties.Settings.Default.Font = this.Font;
+                Properties.Settings.Default.Save();
+                SetFont();
+            }
+        }
+
+        private void tslB_Refresh_Click(object sender, EventArgs e)
+        {
+            ReportDataContext reportDataContext = new ReportDataContext();
+            dataGridView1.DataSource = reportDataContext.sheudle.ToList();
+            SetFont();
+        }
+    }
+}
